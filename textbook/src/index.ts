8c34dc732ee15862bf8bc382fae5d39f38b62519
dab10 <dab10@list.ru>
2022-08-18 02:10:56 +0300

    refactor: refactor code a little

--- textbook/src/index.ts
+++ textbook/src/index.ts
@@ -2,9 +2,9 @@ import './global.css';
 import './global.scss';
 import { Dictionary } from './types';
 
-const base = 'http://localhost:1000';
+const base = 'http://localhost:3500';
 const words = `${base}/words`;
-//let isPlay = false;
+const audio = new Audio();
 
 const getWords = async (page = 0, group = 0): Promise<Dictionary[]> => {
     const response = await fetch(`${words}?page=${page}&group=${group}`);
@@ -49,9 +49,6 @@ const draw = async (page = 0, group = 0): Promise<void> => {
     audioPlayerListener();
 };
 
-draw();
-const audio = new Audio();
-
 const audioPlayerListener = () => {
     const audioElementsArray = document.querySelectorAll('.item__audio');
     console.log(audioElementsArray);
@@ -73,29 +70,23 @@ const playPlaylist = (e: Event) => {
                 audio.src = URL.createObjectURL(blob);
                 return audio.play();
             })
-            .then((_) => {
-                // Video playback started ;)
-                console.log('track');
-            })
-            .catch((e) => {
-                console.log(e);
-                // Video playback failed ;(
+            .catch(() => {
+                audio.removeEventListener('ended', playingTrack);
             });
     };
 
     const playingTrack = () => {
         if (track < 2) {
             console.log('play');
-            // allAudio[track].removeEventListener('ended', playingTrack, false);
             track++;
             console.log(track);
             console.log(allAudio[track].src);
-            // audio.src = allAudio[track].src;
             audio.load();
-            // audio.play();
             fetchAudioAndPlay(track);
         } else {
-            audio.removeEventListener('ended', playingTrack, false);
+            console.log('delete');
+            track = 0;
+            audio.removeEventListener('ended', playingTrack);
         }
     };
 
@@ -104,56 +95,4 @@ const playPlaylist = (e: Event) => {
     audio.play();
 };
 
-// const audioPlayerListener = () => {
-//     const audioElementsArray = document.querySelectorAll('.item__audio');
-//     console.log(audioElementsArray);
-//     console.log(audioElementsArray[0].firstChild);
-//     audioElementsArray.forEach((el) => {
-//         el.addEventListener('click', playPlaylist);
-//     });
-// };
-
-// const playPlaylist = (e: Event) => {
-//     const allAudio = Array.from((e.currentTarget as HTMLDivElement).querySelectorAll('audio'));
-//     let track = 0;
-//     const playingTrack = () => {
-//         if (track === allAudio.length - 1) {
-//             allAudio[track].pause();
-//             console.log('stop');
-//             allAudio[track].removeEventListener('ended', playingTrack);
-//             isPlay = false;
-//         } else {
-//             console.log('play');
-//             allAudio[track].removeEventListener('ended', playingTrack);
-//             track++;
-//             allAudio[track].play();
-//             isPlay = true;
-//         }
-//     };
-
-//     if (!isPlay) {
-//         console.log('enter');
-//         isPlay = true;
-//         console.log(e.currentTarget);
-//         console.log(allAudio);
-//         allAudio[0].play();
-
-//         allAudio.forEach((item) => {
-//             item.addEventListener('ended', playingTrack);
-//         });
-//     } else {
-//         console.log('break');
-//         allAudio[0].removeEventListener('ended', playingTrack);
-//         allAudio[1].removeEventListener('ended', playingTrack);
-//         allAudio[2].removeEventListener('ended', playingTrack);
-//         allAudio.forEach((item) => {
-//             item.pause();
-//             item.currentTime = 0;
-//         });
-
-//         allAudio.forEach((item) => {
-//             item.removeEventListener('ended', playingTrack, false);
-//         });
-//         isPlay = false;
-//     }
-// };
+draw();
