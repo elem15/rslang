1043815d8db72cc53257f45e806fb143191540cf
yurilabatsevich <yurilabatsevich@gmail.com>
2022-08-18 23:24:07 +0300

    feat: update game class, add functions to generate random words

--- audiochallenge/src/core/game.ts
+++ audiochallenge/src/core/game.ts
@@ -1,8 +1,12 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { wordsList } from '../services/words';
+
+import { getWords } from '../services/words';
 import { Word } from '../types';
+import { clear, shuffle } from '../utils';
 import { drawlevels } from '../views/levels';
 import { nextWord } from '../views/next';
+import { showResult } from '../views/result';
+import { nextDefaultText, nextNextText } from './settings';
 
 export default class Game {
     root: HTMLElement;
@@ -16,20 +20,20 @@ export default class Game {
     count: number;
 
     selected: string[];
-    correct?: string[];
-    incorrect?: string[];
+    correct?: string[] = [];
+    incorrect?: string[] = [];
 
     constructor(root: HTMLElement, group?: number) {
         this.root = root;
         this.container = <HTMLElement>document.createElement('div');
         this.progress = <HTMLElement>document.createElement('div');
         this.selected = [];
-        this.count = 0;
+        this.count = 1;
         this.group = group ?? 0;
         this.container.className = 'game';
         this.next = <HTMLButtonElement>document.createElement('button');
         this.next.classList.add('game__next_word');
-        this.next.innerText = `не знаю`;
+        this.next.innerText = nextDefaultText;
         this.next.onclick = async () => this.onNext();
     }
 
@@ -39,21 +43,21 @@ export default class Game {
     };
 
     showLevels = async (): Promise<void> => {
-        await this.clear(this.container);
+        await clear(this.container);
         await drawlevels(this.container, this.onLevelSelect);
     };
 
     onLevelSelect = async (level: number): Promise<void> => {
-        await this.clear(this.container);
+        await clear(this.container);
         this.group = level;
         try {
-            const words = await wordsList(0, this.group);
+            const words = await getWords(0, this.group);
             if (typeof words !== 'undefined') {
                 this.words = words;
                 this.current = await this.getRandomWord();
-                console.log(this.current);
+                const variants = await this.getRandomWords(this.current);
                 this.selected?.push(this.current.id);
-                const variants = this.words.slice(this.count + 1, this.count + 5);
+
                 await nextWord(this.container, this.current, variants);
                 this.container.append(this.next);
                 this.render();
@@ -64,27 +68,53 @@ export default class Game {
     };
 
     onNext = async (): Promise<void> => {
-        this.count += 1;
-        this.current = await this.getRandomWord();
-        console.log(this.current);
-        this.selected.push(this.current.id);
+        if (this.count === 20) {
+            this.next.disabled = true;
+            await this.onEndGame();
+        } else this.count !== this.words.length;
+        {
+            this.count += 1;
+            this.current = await this.getRandomWord();
+            this.selected.push(this.current.id);
+            const variants = await this.getRandomWords(this.current);
 
-        const variants = this.words.slice(this.count + 1, this.count + 5);
-        await this.clear(this.container);
-        await nextWord(this.container, this.current, variants);
-        this.container.append(this.next);
-        this.render();
-    };
-
-    clear = async (container: HTMLElement): Promise<void> => {
-        while (container.firstChild) {
-            container.removeChild(container.firstChild);
+            await clear(this.container);
+            await nextWord(this.container, this.current, variants);
+            this.container.append(this.next);
+            this.render();
         }
     };
 
     render = async (): Promise<void> => {
-        this.clear(this.root);
+        clear(this.root);
         this.root.append(this.progress, this.container);
+        this.next.innerText = nextDefaultText;
+        const answers = Array.from(this.container.querySelectorAll('.answers__item')) as HTMLElement[];
+        answers.forEach((item) => item.addEventListener('click', this.onClickVariant));
+    };
+
+    onClickVariant = async (e: MouseEvent): Promise<void> => {
+        const target = e.target as HTMLElement;
+        this.wordCardUpdate();
+
+        if (target.closest('.answers__item') && target.innerText === this.current?.wordTranslate) {
+            const label = target.parentElement;
+            label?.classList.add('correct');
+            this.correct?.push(this.current.id);
+        } else {
+            const label = target.parentElement;
+            label?.classList.add('incorrect');
+            const answers = Array.from(this.container.querySelectorAll('.answers__item')) as HTMLElement[];
+            answers.forEach((item) => item.removeEventListener('click', this.onClickVariant));
+            if (this.current) this.incorrect?.push(this.current.id);
+        }
+    };
+
+    onEndGame = async (): Promise<void> => {
+        this.next.disabled = true;
+        const correct = this.words.filter((item) => this.correct?.includes(item.id));
+        const incorrect = this.words.filter((item) => this.incorrect?.includes(item.id));
+        showResult(correct, incorrect);
     };
 
     getRandomWord = async (): Promise<Word> => {
@@ -94,10 +124,20 @@ export default class Game {
         return filtered[index];
     };
 
-    getRandomWords = async (exclude: Word): Promise<Word[]> => {
+    getRandomWords = async (current: Word): Promise<Word[]> => {
         const arr: Word[] = [];
-        arr.push(exclude);
+        arr.push(current);
+        while (arr.length < 4) {
+            const index = Math.floor(Math.random() * this.words.length);
+            const next = this.words[index];
+            if (!arr.includes(next)) arr.push(next);
+        }
+        return shuffle(arr);
+    };
 
-        return arr;
+    wordCardUpdate = () => {
+        const image = this.container.querySelector('.word__image img') as HTMLImageElement;
+        this.next.innerText = nextNextText;
+        image.style.display = 'block';
     };
 }
