52ba0ccbdba1da967194c8a29bcc4762ec00293c
yurilabatsevich <yurilabatsevich@gmail.com>
2022-08-20 17:03:05 +0300

    refactor: update utild

--- audiochallenge/src/utils/index.ts
+++ audiochallenge/src/utils/index.ts
@@ -1,14 +1,14 @@
 export const shuffle = <T>(collection: T[]): T[] => {
-  const arr = collection.slice();
-  for (let i = arr.length - 1; i > 0; i--) {
-      const rand = Math.floor(Math.random() * (i + 1));
-      [arr[i], arr[rand]] = [arr[rand], arr[i]];
-  }
-  return arr;
+    const arr = collection.slice();
+    for (let i = arr.length - 1; i > 0; i--) {
+        const rand = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[rand]] = [arr[rand], arr[i]];
+    }
+    return arr;
 };
 
 export const clear = async (container: HTMLElement): Promise<void> => {
-  while (container.firstChild) {
-      container.removeChild(container.firstChild);
-  }
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 };
