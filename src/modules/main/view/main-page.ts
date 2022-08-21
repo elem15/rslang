ba884f0d59522f5cdd9ab3299c4dd15833f6c51f
Elem <elem15@yandex.ru>
2022-08-21 23:50:56 +0300

    feat: background image

--- src/modules/main/view/main-page.ts
+++ src/modules/main/view/main-page.ts
@@ -1,8 +1,16 @@
+import '../scss/styles.scss';
+import img from '../../../images/DSC02301.jpg';
+
 export const renderMainPage = () => {
     const root = document.getElementById('root');
     while (root.lastChild) root.lastChild.remove();
     const main = document.createElement('section');
     main.className = 'main';
-    main.innerHTML = 'Main page';
+    main.innerHTML = '<div class="main-content">Main page</div>';
     root.append(main);
+    const background = document.createElement('div');
+    background.innerHTML = `
+        <img class="background" src=${img}> 
+    `;
+    main.append(background);
 };
