5ac720b8aa83bc6eecd3384de23c10356eae963f
Elem <elem15@yandex.ru>
2022-08-21 16:12:45 +0300

    feat: create footer

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import './scss/styles.scss';
 import { signUser } from './modules/auth/services/sign-user';
 import { renderPage } from './modules/router/services/router';
 import { setNavEvents } from './modules/router/controllers/nav-events';
+import { footerViews } from './modules/auth/view/footer';
 
+footerViews();
 signUser();
 setNavEvents();
 renderPage(null, null);
