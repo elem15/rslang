d4400f14adc525ed0e6eec781f55e1cb223fd4a2
yurilabatsevich <yuralabatsevich@gmail.com>
2022-08-22 15:06:13 +0300

    refactor: remove some methods from game class, add export function to utils

--- audiochallenge/src/utils/index.ts
+++ audiochallenge/src/utils/index.ts
@@ -1,3 +1,5 @@
+import { Word } from '../types';
+
 export const shuffle = <T>(collection: T[]): T[] => {
     const arr = collection.slice();
     for (let i = arr.length - 1; i > 0; i--) {
@@ -11,4 +13,22 @@ export const clear = async (container: HTMLElement): Promise<void> => {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
+};
+
+export const getRandomWords = async (excludeWord: Word, words: Word[]): Promise<Word[]> => {
+    const arr: Word[] = [];
+    arr.push(excludeWord);
+    while (arr.length < 4) {
+        const index = Math.floor(Math.random() * words.length);
+        const next = words[index];
+        if (!arr.includes(next)) arr.push(next);
+    }
+    return shuffle(arr);
+};
+
+export const getRandomWord = async (selected: string[], words: Word[]): Promise<Word> => {
+    const filtered = words.filter((word) => !selected.includes(word.id));
+    const index = Math.floor(Math.random() * (words.length - selected.length));
+
+    return filtered[index];
 };
