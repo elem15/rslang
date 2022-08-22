d0fd83c13d5f6c7ee68075de06226930d87819ea
yurilabatsevich <yuralabatsevich@gmail.com>
2022-08-22 15:10:47 +0300

    feat:add random generator group of words before beginning of game

--- audiochallenge/src/index.ts
+++ audiochallenge/src/index.ts
@@ -2,8 +2,10 @@ import 'normalize.css';
 import './assets/styles/main.scss';
 import Game from './core/game';
 
-(async () => {
+const group = Math.floor(Math.random() * 6);
+
+(async (group?: number) => {
     const root = <HTMLElement>document.querySelector('.app');
-    const game = new Game(root);
+    const game = new Game(root, group);
     await game.start();
-})();
+})(group);
