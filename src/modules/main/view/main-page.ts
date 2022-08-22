abf3ddfc87d9a62f0653f7d9d5f292005caa9f23
Elem <elem15@yandex.ru>
2022-08-22 21:56:59 +0300

    feat: add headers and avatars to main page

--- src/modules/main/view/main-page.ts
+++ src/modules/main/view/main-page.ts
@@ -1,4 +1,5 @@
 import '../scss/styles.scss';
+import yuri from '../../../images/yuri.jpg';
 import img from '../../../images/DSC02301.jpg';
 import logoImage from '../../../images/rs_school_js.svg';
 import gitHubImage from '../../../images/git_hub.svg';
@@ -44,7 +45,56 @@ export const renderMainPage = () => {
     while (root.lastChild) root.lastChild.remove();
     const main = document.createElement('section');
     main.className = 'main';
-    main.innerHTML = '<div class="main-content">Main page</div>';
+    main.innerHTML = `
+    <div class="main-content">
+    <div class="container">
+      <div class="col-12 text-end">
+        <blockquote class="blockquote">“Границы моего языка означают границы моего мира”</blockquote>
+        <div class="blockquote-footer">
+        <cite title="Source Title "> Людвиг Витгенштейн</cite>
+        </div>
+      </div>
+      <h1 class="h1">lέxi</h1>
+      <div class="">
+          <cite title="Source Title">— изучай язык...</cite>
+          <cite title="Source Title">открывай мир...</cite>
+      </div>
+      <br>
+      <br>
+      <p class="about text-end">В приложени <strong>lexi</strong> вы можете изучачить 3600 часто употребляемых английских слов. <br>В этом вам поможет <strong>Учебник</strong> с ассициативными карточками. А так же 2 замечательные игры <strong>Аудиовызов</strong> и <strong>Спринт</strong></p>
+    </div>
+    <div class="container avatars">
+    <div class="row justify-content-between">  
+      <ul class="main__list nav justify-content-evenly">     
+        <li class="main__list-item nav-item">
+          <a class="git-link nav-link" href="https://github.com/dab10">
+          <figure class="figure denis">
+          <div class="avatar"></div>
+          <figcaption class="figure-caption text-center">Денис Баженов</figcaption>
+          </figure>
+          </a>
+        </li>
+        <li class="main__list-item nav-item">
+          <a class="git-link nav-link" href="https://github.com/labatsevich">
+          <figure class="figure yuri">
+            <img src=${yuri} class="figure-img avatar img-fluid" alt="...">
+            <figcaption class="figure-caption text-center">Юрий Лабацевич</figcaption>
+          </figure>
+          </a>
+        </li>
+        <li class="main__list-item nav-item">
+          <a class="git-link nav-link" href="https://github.com/elem15">          
+          <figure class="figure mikhail">
+          <div class="avatar"></div>
+          <figcaption class="figure-caption text-center">Михаил Дворкин</figcaption>
+          </figure>
+          </a>
+        </li>    
+      </ul>
+    </div>
+  </div>
+    </div>
+    `;
     root.append(main);
     const background = document.createElement('div');
     background.innerHTML = `
