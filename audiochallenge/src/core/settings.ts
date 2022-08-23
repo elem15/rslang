91e362ee7e77846cf5d44819719e3f3c49f9a8a8
yurilabatsevich <yurilabatsevich@gmail.com>
2022-08-24 01:22:49 +0300

    feat: add circle diagram

--- audiochallenge/src/core/settings.ts
+++ audiochallenge/src/core/settings.ts
@@ -9,6 +9,17 @@ export const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" hei
 export const wrongIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
 </svg>`;
+export const statisticCircleGraph = `<svg viewBox="0 0 36 36">
+<path
+  d="M18 2.0845
+    a 15.9155 15.9155 0 0 1 0 31.831
+    a 15.9155 15.9155 0 0 1 0 -31.831"
+  fill="none"
+  stroke="#444";
+  stroke-width="1";
+/>
+</svg>`;
+
 export const nextDefaultText = 'не знаю';
 export const nextNextText = 'следующее';
 export const successPath = 'assets/sounds/success.wav';
