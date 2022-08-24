5ec914129f028323def5ad8ebd13dcbd39f3f0f5
Elem <elem15@yandex.ru>
2022-08-24 13:31:20 +0300

    feat: connect to external server

--- src/modules/auth/controllers/hosts.ts
+++ src/modules/auth/controllers/hosts.ts
@@ -1,2 +1,4 @@
 // export const host = 'https://react-learnwords-example.herokuapp.com';
-export const host = 'http://localhost:3500';
+// export const host = 'http://localhost:3500';
+// export const host = 'https://lexi-rslang.herokuapp.com';
+export const host = 'https://lexi-rs-school.herokuapp.com';
