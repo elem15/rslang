90298c9cd156fc63d0c2b041de1731ce2a809a8a
yurilabatsevich <yuralabatsevich@gmail.com>
2022-08-25 21:45:05 +0300

    refactor: change  host address from dev to prod

--- src/modules/auth/controllers/hosts.ts
+++ src/modules/auth/controllers/hosts.ts
@@ -1,2 +1,1 @@
-// export const host = 'https://react-learnwords-example.herokuapp.com';
-export const host = 'http://localhost:3500';
+export const host = 'https://react-learnwords-example.herokuapp.com';
