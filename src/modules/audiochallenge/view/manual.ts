e6d48062f03acb7ab7bc2a3fb3e02ccf63afd6db
yurilabatsevich <yuralabatsevich@gmail.com>
2022-08-26 16:16:51 +0300

    feat: add game manual

---
+++ src/modules/audiochallenge/view/manual.ts
@@ -0,0 +1,11 @@
+export const showManual = (): void => {
+    const game = document.querySelector('.game') as HTMLElement;
+    const container = document.createElement('div');
+
+    container.className = 'game__manual';
+    container.innerHTML = `<p>Выберите верный вариант перевода из предложенных слов,
+    используя клавишы <b>1</b>, <b>2</b>, <b>3</b>, <b>4</b>.<p>
+    <p><strong>Space</strong>, чтобы воспроизвести слово, <strong>Enter</strong> для перехода
+    к следующему слову, после того, как сделали выбор, либо просто используйте мышку.</p>`;
+    game.prepend(container);
+};
