c07aee8349787372716c0d2c8b42d614910c92e2
dab10 <dab10@list.ru>
2022-08-26 03:24:09 +0300

    refactor: delete some functions

--- src/modules/words/controllers/get-word-by-id.ts
+++ src/modules/words/controllers/get-word-by-id.ts
@@ -1,14 +1,14 @@
-import * as a from 'axios';
-import { messageModal } from '../../auth/view/message-modal';
-import { host } from '../../auth/controllers/hosts';
+// import * as a from 'axios';
+// import { messageModal } from '../../auth/view/message-modal';
+// import { host } from '../../auth/controllers/hosts';
 
-const axios = a.default;
+// const axios = a.default;
 
-export const getWordById = async (wordId: string): Promise<string> => {
-    try {
-        const response = await axios.get(`${host}/words/${wordId}`);
-        return response.data.word;
-    } catch {
-        messageModal('Word not exist');
-    }
-};
+// export const getWordById = async (wordId: string): Promise<string> => {
+//     try {
+//         const response = await axios.get(`${host}/words/${wordId}`);
+//         return response.data.word;
+//     } catch {
+//         messageModal('Word not exist');
+//     }
+// };
