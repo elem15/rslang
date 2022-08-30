ab2845f6b26d7a4ce3211a7e2e16a6e03333fb1e
yurilabatsevich <yuralabatsevich@gmail.com>
2022-08-30 16:25:28 +0300

    feat: add functions to work with api

--- src/modules/audiochallenge/services/words.ts
+++ src/modules/audiochallenge/services/words.ts
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Word } from '../../../types';
+import { UserWords } from '../../../types/textbook-types';
 import { UserData } from '../../../types/user-types';
 import { host } from '../../auth/controllers/hosts';
+import { formateDate } from '../utils';
 
 export const getWords = async (page = 0, group: number): Promise<Word[] | undefined> => {
     const response = await fetch(`${host}/words?page=${page}&group=${group}`);
@@ -26,5 +29,90 @@ export const getUserWordsByDifficulty = async (difficulty = 'hard', wordsPerPage
         return words[0].paginatedResults;
     } catch (err) {
         console.log(err);
+    }
+};
+
+export const getUserWords = async (): Promise<UserWords[]> => {
+    const user: UserData = JSON.parse(localStorage.getItem('data'));
+    const { userId, token } = user;
+    try {
+        const response = await fetch(`${host}/users/${userId}/words`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: 'application/json',
+            },
+        });
+
+        return await response.json();
+    } catch (Exception) {
+        console.error(Exception);
+    }
+};
+
+export const getUserWord = async (wordID: string): Promise<any> => {
+    const user: UserData = JSON.parse(localStorage.getItem('data'));
+    const { userId, token } = user;
+
+    try {
+        const response = await fetch(`${host}/users/${userId}/words/${wordID}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: 'application/json',
+            },
+        });
+        if (response.status === 200) return await response.json();
+        return undefined;
+    } catch (Exception) {
+        console.error(Exception);
+    }
+};
+
+export const upsertWord = async (wordID: string): Promise<any> => {
+    const user: UserData = JSON.parse(localStorage.getItem('data'));
+    const { userId, token } = user;
+    const word = await getUserWord(wordID);
+
+    if (!word) {
+        const body = JSON.stringify({
+            difficulty: 'neutral',
+            optional: {
+                date: formateDate(new Date()),
+                isWordNew: true,
+            },
+        });
+        const response = await fetch(`${host}/users/${userId}/words/${wordID}`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: 'application/json',
+                'Content-type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+            body: body,
+        });
+
+        return await response.json();
+    } else {
+        if (!('optional' in word) || !('isNewWord' in word.optional)) {
+            const body = JSON.stringify({
+                optional: {
+                    date: formateDate(new Date()),
+                    isWordNew: true,
+                },
+            });
+
+            const response = await fetch(`${host}/users/${userId}/words/${wordID}`, {
+                method: 'PUT',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    Accept: 'application/json',
+                    'Content-type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                },
+                body: body,
+            });
+
+            return await response.json();
+        }
     }
 };
