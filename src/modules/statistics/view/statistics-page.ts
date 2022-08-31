e5fa5518930b8913da7d6cda3763ad2e52ea3312
dab10 <dab10@list.ru>
2022-09-01 01:26:16 +0300

    fix: prepare for merge

--- src/modules/statistics/view/statistics-page.ts
+++ src/modules/statistics/view/statistics-page.ts
@@ -1,6 +1,4 @@
-// import { Optional, Pages, Result } from '../../../types/textbook-types';
 import { removeFooter } from '../../main/view/main-page';
-import { addNewWord, addResultGame, getCountNewWords } from '../services/api';
 
 export const renderStatisticsPage = async () => {
     const root = document.getElementById('root');
@@ -10,59 +8,4 @@ export const renderStatisticsPage = async () => {
     statistics.className = 'statistics';
     statistics.innerHTML = 'Statistics';
     root.append(statistics);
-
-    workWithShortStatistic();
-};
-
-const workWithShortStatistic = async () => {
-    // let pageLearnedObjectArrayType: Pages;
-    // let pageLearnedObject: Optional;
-    // let isAuthorization = true;
-    //     if (isAuthorization) {
-
-    //         const pageLearnedResponse = await getSettings();
-    //         if (pageLearnedResponse) {
-    //             pageLearnedObject = pageLearnedResponse.optional;
-    //             if (pageLearnedObject) {
-    //                 pageLearnedObjectArrayType = pageLearnedObject.pages
-    //         }
-    //     }
-    // }
-    //     console.log(pageLearnedObjectArrayType);
-
-    // let resultGame: Result = {
-    //     date: new Date().toLocaleDateString('ru-RU'),
-    //     newWords: 0,
-    //     rightAnswers: 0,
-    //     wrongAnswers: 0,
-    //     longestSeries: 0
-    // }
-
-    // let shortStatistic = {
-    //     optional: {
-    //         pages: pageLearnedObjectArrayType,
-    //         gameStatistics: {
-    //             sprint: resultGame,
-    //             audiochallenge: resultGame
-    //         }
-    //     }
-    // }
-
-    // await updateSettings(shortStatistic);
-
-    // if (isAuthorization) {
-
-    //     const pageLearnedResponse = await getSettings();
-    //     let pageLearnedObject: Optional;
-    //     if (pageLearnedResponse) {
-    //         pageLearnedObject = pageLearnedResponse.optional;
-    //     }
-    //     console.log(pageLearnedResponse);
-    // }
-
-    await addNewWord('5e9f5ee35eb9e72bc21af4c4', 0, 1, true);
-
-    await addResultGame('audiochallenge', 1, 2, 3, 4);
-
-    console.log(await getCountNewWords());
 };
