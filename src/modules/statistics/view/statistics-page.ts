259e2516fdf1cb98a3afa65e3c9baaa5ad6dc2d6
dab10 <dab10@list.ru>
2022-09-01 16:06:18 +0300

    init: start statistic

--- src/modules/statistics/view/statistics-page.ts
+++ src/modules/statistics/view/statistics-page.ts
@@ -1,4 +1,6 @@
+// import { Optional, Pages, Result } from '../../../types/textbook-types';
 import { removeFooter } from '../../main/view/main-page';
+import { addNewWord, addResultGame, getCountNewWords } from '../services/api';
 
 export const renderStatisticsPage = async () => {
     const root = document.getElementById('root');
@@ -8,4 +10,15 @@ export const renderStatisticsPage = async () => {
     statistics.className = 'statistics';
     statistics.innerHTML = 'Statistics';
     root.append(statistics);
+
+    workWithShortStatistic();
+};
+
+const workWithShortStatistic = async () => {
+
+    await addNewWord('5e9f5ee35eb9e72bc21af4c4', 0, 1, true);
+
+    await addResultGame('audiochallenge', 1, 2, 3, 4);
+
+    console.log(await getCountNewWords());
 };
