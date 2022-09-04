7665f2e42b6577d85c24fa7cd14346d24b8a8e21
dab10 <dab10@list.ru>
2022-09-04 18:19:08 +0300

    refactor: correct styles

--- src/modules/words/view/words-list.ts
+++ src/modules/words/view/words-list.ts
@@ -1,4 +1,4 @@
-import { renderFooter } from '../../main/view/main-page';
+import { removeFooter } from '../../main/view/main-page';
 import { startTextbook } from './startTextbook';
 
 export const renderWordsList = async (): Promise<void> => {
@@ -15,7 +15,7 @@ export const renderWordsList = async (): Promise<void> => {
 
     const root = document.getElementById('root');
     while (root.lastChild) root.lastChild.remove();
-    renderFooter();
+    removeFooter();
     const words = document.createElement('section');
     words.className = 'dictionary';
     root.append(words);
