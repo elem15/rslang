2a1849ca510a7dc2c4ac7fc6993d5cfdf2c1174c
Elem <elem15@yandex.ru>
2023-06-18 19:15:35 +0300

    feat: add backend new url

--- src/modules/auth/controllers/hosts.ts
+++ src/modules/auth/controllers/hosts.ts
@@ -1,2 +1,3 @@
 // export const host = 'http://localhost:3500';
-export const host = 'https://rs-lang-l4aq.onrender.com';
+// export const host = 'https://rs-lang-l4aq.onrender.com';
+export const host = 'https://react-rslang-be-production-971f.up.railway.app/'
